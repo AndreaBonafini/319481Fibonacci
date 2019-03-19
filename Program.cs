07c88f95851d81d55406981a2b81077315efd12d
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-03-19 09:59:24 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            int i, count, f1 = 0, f2 = 1, f3 = 0;
+            Console.WriteLine("Enter a limit: ");
+
+            count = int.Parse(Console.ReadLine());
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+
+            for(i = 0;i <count; i++)
+            {
+                f3 = f1 + f2;
+                Console.WriteLine(f3);
+                f1 = f2;
+                f2 = f3;
+            }
+            Console.ReadLine();
         }
     }
 }
